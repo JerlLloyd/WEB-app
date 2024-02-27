9649f44e4dfb8e487d1b24b9842934a660155335
JerlLloyd <garciajerlloyd@gmail.com>
2024-02-27 15:02:02 +0800

    changes

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,7 +133,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-django_heroku.settings(locals())
+
 
 MEDIA_URL= '/images/'
 
